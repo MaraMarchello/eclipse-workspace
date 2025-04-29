7a1a7c3386161285c3fd6c34e4395d29130da373
MaraMarchello <maramarchelo@gmail.com>
2025-04-29 11:25:35 +0200

    Test commit

---
+++ virtual-escape-room/virtual-escape-room/src/main/java/com/escaperoom/virtual/VirtualEscapeRoomApplication.java
@@ -0,0 +1,13 @@
+package com.escaperoom.virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualEscapeRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualEscapeRoomApplication.class, args);
+	}
+
+}
